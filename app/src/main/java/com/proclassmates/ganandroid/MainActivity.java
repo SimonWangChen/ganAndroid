d146e555b11a4995dcd0d86a98e102aff415cc14
SimonWangChen <hrbwangchen90@163.com>
2019-10-31 13:07:43 +0800

    Animation Drawable

--- app/src/main/java/com/proclassmates/ganandroid/MainActivity.java
+++ app/src/main/java/com/proclassmates/ganandroid/MainActivity.java
@@ -2,13 +2,22 @@ package com.proclassmates.ganandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView iv_guide_point_1;
 
+    private ImageView iv_guide_star;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_pager_guide_1);
+
+        iv_guide_star = findViewById(R.id.iv_guide_star);
+        //播放帧动画
+        AnimationDrawable animStar = (AnimationDrawable) iv_guide_star.getBackground();
+        animStar.start();
     }
 }
