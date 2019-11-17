ba177f4740fd736bdce840de0e989cf4c164dd6e
SimonWangChen <hrbwangchen90@163.com>
2019-11-17 15:12:23 +0800

    static fragment

---
+++ app/src/main/java/com/proclassmates/ganandroid/fragment/ContentFragment.java
@@ -0,0 +1,13 @@
+package com.proclassmates.ganandroid.fragment;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * @name: GanAndroid
+ * @desc:
+ * @class name:com.proclassmates.ganandroid.fragment
+ * @author: simon
+ * @time： 2019-11-17 14:59
+ */
+public class ContentFragment extends Fragment {
+}
---
+++ app/src/main/java/com/proclassmates/ganandroid/fragment/TitleFragment.java
@@ -0,0 +1,11 @@
+package com.proclassmates.ganandroid;
+
+/**
+ * @name: GanAndroid
+ * @desc:
+ * @class name:com.proclassmates.ganandroid
+ * @author: simon
+ * @time： 2019-11-17 14:48
+ */
+public class TitleFragment {
+}
