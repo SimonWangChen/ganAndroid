632ecb7d159e74aea89eda5d907abe9c9964dba4
SimonWangChen <hrbwangchen90@163.com>
2019-11-17 16:39:16 +0800

    init fragment v2

--- app/src/main/java/com/proclassmates/ganandroid/MainActivity.java
+++ app/src/main/java/com/proclassmates/ganandroid/MainActivity.java
@@ -10,27 +10,27 @@ import android.os.Bundle;
 import com.proclassmates.ganandroid.fragment.ContentFragment;
 import com.proclassmates.ganandroid.fragment.TitleFragment;
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dynamic_main);
-
-        /**
-         * 1.创建Fragment 管理器对象
-         * 2.获取 fragment 事物对象
-         * 3.fragment 事物对象对 fragment 进行管理
-         * 4.提交事物
-         */
-        FragmentManager fragmentManager = getFragmentManager();
-
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        fragmentTransaction.add(R.id.title_layout, new TitleFragment());
-        fragmentTransaction.add(R.id.content_layout, new ContentFragment());
-        fragmentTransaction.commit();
-
-
-    }
-}
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_dynamic_main);
+//
+//        /**
+//         * 1.创建Fragment 管理器对象
+//         * 2.获取 fragment 事物对象
+//         * 3.fragment 事物对象对 fragment 进行管理
+//         * 4.提交事物
+//         */
+//        FragmentManager fragmentManager = getFragmentManager();
+//
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//
+//        fragmentTransaction.add(R.id.title_layout, new TitleFragment());
+//        fragmentTransaction.add(R.id.content_layout, new ContentFragment());
+//        fragmentTransaction.commit();
+//
+//
+//    }
+//}
