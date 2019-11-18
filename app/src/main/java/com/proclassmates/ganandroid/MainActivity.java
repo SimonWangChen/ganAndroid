6db20717209957e73c9999fa37e94c31fada1c97
SimonWangChen <hrbwangchen90@163.com>
2019-11-18 20:09:09 +0800

    添加不同的 layout 布局

--- app/src/main/java/com/proclassmates/ganandroid/MainActivity.java
+++ app/src/main/java/com/proclassmates/ganandroid/MainActivity.java
@@ -1,8 +1,10 @@
 package com.proclassmates.ganandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
 
@@ -26,11 +28,15 @@ public class MainActivity extends AppCompatActivity {
 
         mAdapter = new SimpleListAdapter(this, mDatas);
         mRecyclerView.setAdapter(mAdapter);
-        //设置布局
+        //  设置布局 线型布局
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        // 网格布局
+        // GridLayoutManager linearLayoutManager = new GridLayoutManager(this, 2);
+        // 水平滑动布局
+        // StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(5, StaggeredGridLayoutManager.HORIZONTAL);
         mRecyclerView.setLayoutManager(linearLayoutManager);
         //设置分割线 https://gist.github.com/alexfu/0f464fc3742f134ccd1e
-        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
+        //mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
 
     }
 
